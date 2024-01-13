f5351b5f860d6f24f69f3c0601288a7cf6f8de81
mwallace98 <mattwallace140@gmail.com>
2024-01-13 14:13:56 -0500

    all tets passing

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -33,11 +33,10 @@ router.put(
   '/:id',
    md.checkAccountId,
    md.checkAccountPayload,
-   md.checkAccountNameUnique,
-     (req, res, next) => {
+  async (req, res, next) => {
   try{
-  
-    res.json('update account')
+    const updated = await Account.updateById(req.params.id,req.body)
+    res.json(updated)
   } catch(err){
     next(err)
   }
